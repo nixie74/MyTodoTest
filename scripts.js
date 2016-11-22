148063d60a3df31175bc6a0f35905916dae1d8ad
afig <afig23@gmail.com>
2016-11-21 22:48:12 -0500

    Add proper handling of empty fields; font changes

--- scripts.js
+++ scripts.js
@@ -2,8 +2,20 @@ function addNewTask(){
 	var taskName = document.getElementById("taskInput").elements["task"].value;
 	var dueDate = document.getElementById("taskInput").elements["dueDate"].value;
 	var dueTime = document.getElementById("taskInput").elements["dueTime"].value;
-	var currList = document.getElementById("taskList").innerHTML;
-	var newList = currList + "<br></br>" + "<b>Task:</b> " + taskName + " <b>Due:</b>" +
-	    dueDate + " at " + dueTime;
-	document.getElementById("taskList").innerHTML = newList;
+	if (taskName == ""){
+		document.getElementById("warn").style.visibility = "visible";
+	}
+	else{
+		document.getElementById("warn").style.visibility = "hidden";
+		var currList = document.getElementById("taskList").innerHTML;
+		var newList = currList + "<br></br>" + "<b>Task:</b> " + taskName;
+		if (dueDate != ""){
+			newList += " <b>Due:</b>" + dueDate;
+			if (dueTime != ""){
+				newList += " at " + dueTime;
+			}
+		}
+		document.getElementById("taskList").innerHTML = newList;
+	}
+
 }
