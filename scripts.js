d8e967bb0c61d0689e94fae20379b7dd1294447c
afig <afig23@gmail.com>
2016-11-26 20:08:38 -0500

    Represent each task as an object; implement jquery

--- scripts.js
+++ scripts.js
@@ -1,21 +1,40 @@
-function addNewTask(){
-	var taskName = document.getElementById("taskInput").elements["task"].value;
-	var dueDate = document.getElementById("taskInput").elements["dueDate"].value;
-	var dueTime = document.getElementById("taskInput").elements["dueTime"].value;
-	if (taskName == ""){
-		document.getElementById("warn").style.visibility = "visible";
-	}
-	else{
-		document.getElementById("warn").style.visibility = "hidden";
-		var currList = document.getElementById("taskList").innerHTML;
-		var newList = currList + "<br></br>" + "<b>Task:</b> " + taskName;
-		if (dueDate != ""){
-			newList += " <b>Due:</b>" + dueDate;
-			if (dueTime != ""){
-				newList += " at " + dueTime;
-			}
-		}
-		document.getElementById("taskList").innerHTML = newList;
-	}
+$(function(){
+  var taskList = [];
 
+  document.getElementById("numTasks").innerHTML = taskList.length;
+  $("#addTask").click(function(){
+		var newTask = getNewTask();
+
+		if (!newTask.text){
+			document.getElementById("warn").style.visibility = "visible";
+			return;
+		}
+
+		document.getElementById("warn").style.visibility = "hidden";
+    taskList.push(newTask);
+
+    //newItem represents a new <p> element to be added to document
+    var newItem = "<p id='task" + taskList.length + "'>";
+    newItem += "<b>Task:</b> " + newTask.text;
+    if (newTask.dueDate){
+      newItem += " <b>Due:</b> " + newTask.dueDate;
+      if (newTask.dueTime){
+        newItem += " at " + newTask.dueTime;
+      }
+    }
+    newItem += "</p>";
+
+    $("#todoList").append(newItem);
+		document.getElementById("numTasks").innerHTML = taskList.length;
+  })
+});
+
+function getNewTask(){
+	//returns a task object {text:string, dueDate:string, dueTime:string}
+	//all three members should be not undefined (but could be otherwise falsy)
+	var fTaskName = document.getElementById("taskInput").elements["task"].value;
+	var fDueDate = document.getElementById("taskInput").elements["dueDate"].value;
+	var fDueTime = document.getElementById("taskInput").elements["dueTime"].value;
+
+	return {text:fTaskName, dueDate:fDueDate, dueTime:fDueTime};
 }
